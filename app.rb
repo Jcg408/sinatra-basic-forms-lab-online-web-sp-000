e319ab3b145f4e282dce75c2a5378a0201a7dad6
Jessica Gladstone <jcg321@comcast.net>
2019-02-14 20:05:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,23 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  get '/' do
-    
-    erb :index
-  end
-  
-  get '/new'
-    erb :create_puppy
-  end
-  
-  post '/puppy' do
-    pup = Puppy.new(params[:name], params[:breed], params[:age])
-    @name = pup.name
-    @breed = pup.breed
-    @age = pup.age    
 
-    erb :display_puppy
-  end
+    get '/' do
+        erb :index
+    end
+
+    get '/new' do
+        erb :create_puppy
+    end 
+
+    post '/puppy' do
+        pup = Puppy.new(params[:name], params[:breed], params[:age])
+        @name = pup.name
+        @breed = pup.breed
+        @age = pup.age    
+
+        erb :display_puppy
+    end
+
 
 end
